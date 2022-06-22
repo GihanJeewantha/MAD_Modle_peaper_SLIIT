862057d5d46ade7b10c6ee563a3b373026f4ac61
Gihan Jeewantha <dilshanj46@gmail.com>
2022-06-22 23:38:36 +0530

    create update insert activity

---
+++ app/src/main/java/com/example/madpeapersliit/profileManagement.java
@@ -0,0 +1,14 @@
+package com.example.madpeapersliit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profileManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_management);
+    }
+}
